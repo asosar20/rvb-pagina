209cb44ee799ec0b3fa4101f543d12b57d05d7ca
asosar20 <asosar20@gmail.com>
2025-09-30 17:41:27 -0500

    Update Whatsapp.jsx

--- src/components/whatsapp/Whatsapp.jsx
+++ src/components/whatsapp/Whatsapp.jsx
@@ -4,7 +4,7 @@ const Whatsapp = () => {
     return (
         <div>
             <a
-                href="https://wa.me/51912345678"
+                href="https://wa.me/51907981164"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="fixed bottom-6 right-6 bg-green-500 hover:bg-green-600 text-white w-14 h-14 flex items-center justify-center rounded-full shadow-lg transition-transform duration-300 hover:scale-110 z-50"
@@ -20,3 +20,4 @@ const Whatsapp = () => {
 }
 
 export default Whatsapp
+
